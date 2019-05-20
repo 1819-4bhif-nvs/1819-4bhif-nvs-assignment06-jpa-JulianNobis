c8a893a3ea3d7456b8d8adbba7edbac6ac7c2901
JulianNobis <julian09022001@gmail.com>
2019-05-20 20:28:48 +0200

    added functionality for singles.java and doubles.java (this is their shared endpoint)

--- Tennisclubadministration/src/main/java/at/htl/tennisclubadministration/rest/TennismatchEndpoint.java
+++ Tennisclubadministration/src/main/java/at/htl/tennisclubadministration/rest/TennismatchEndpoint.java
@@ -1,5 +1,7 @@
 package at.htl.tennisclubadministration.rest;
 
+import at.htl.tennisclubadministration.model.Doubles;
+import at.htl.tennisclubadministration.model.Singles;
 import at.htl.tennisclubadministration.model.Tennismatch;
 
 import javax.persistence.EntityManager;
@@ -27,6 +29,34 @@ public class TennismatchEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     public Tennismatch find(@PathParam("id") Long id){
         return em.find(Tennismatch.class, id);
+    }
+
+    @Path("find/singles/all")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Singles> findAllSingles(){
+        return em.createNamedQuery("Singles.findAll", Singles.class).getResultList();
+    }
+
+    @Path("find/doubles/all")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Doubles> findAllDoubles(){
+        return em.createNamedQuery("Doubles.findAll", Doubles.class).getResultList();
+    }
+
+    @Path("find/singles/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Singles findSingles(@PathParam("id") Long id){
+        return em.find(Singles.class, id);
+    }
+
+    @Path("find/doubles/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Doubles findDoubles(@PathParam("id") Long id){
+        return em.find(Doubles.class, id);
     }
 
 }
