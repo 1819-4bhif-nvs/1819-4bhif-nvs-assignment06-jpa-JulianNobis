54f789ac7483bc9cd578fc9e98a119dc68b7077b
JulianNobis <julian09022001@gmail.com>
2019-05-20 20:26:01 +0200

    changed structure of doubles.java

--- Tennisclubadministration/src/main/java/at/htl/tennisclubadministration/model/Doubles.java
+++ Tennisclubadministration/src/main/java/at/htl/tennisclubadministration/model/Doubles.java
@@ -3,19 +3,34 @@ package at.htl.tennisclubadministration.model;
 import javafx.util.Pair;
 
 import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Entity
+@XmlRootElement
+@NamedQueries( {
+        @NamedQuery(name = "Doubles.findAll", query = "select d from Doubles d"),
+        @NamedQuery(name = "Doubles.findById", query = "select d from Doubles d where d.id = ?1"),
+        @NamedQuery(name = "Doubles.getWinner", query = "select d.winner from Doubles d")
+})
 public class Doubles extends Tennismatch {
 
-    private Pair<Tennisplayer, Tennisplayer> players1 = new Pair<>(new Tennisplayer(), new Tennisplayer());
-    private Pair<Tennisplayer, Tennisplayer> players2 = new Pair<>(new Tennisplayer(), new Tennisplayer());
-    private Pair<Tennisplayer, Tennisplayer> winner = new Pair<>(new Tennisplayer(), new Tennisplayer());
+    @OneToMany
+    private Map<Tennisplayer, Tennisplayer> players1 = new LinkedHashMap();
+    @OneToMany
+    private Map<Tennisplayer, Tennisplayer> players2 = new LinkedHashMap<>();
+    @OneToMany
+    private Map<Tennisplayer, Tennisplayer> winner = new LinkedHashMap<>();
 
     // region Constructor
     public Doubles() { }
 
-    public Doubles(LocalDate localDate, int duration, String score, Pair<Tennisplayer, Tennisplayer> players1, Pair<Tennisplayer, Tennisplayer> players2, Pair<Tennisplayer, Tennisplayer> winner) {
+    public Doubles(LocalDate localDate, int duration, String score, Map<Tennisplayer, Tennisplayer> players1, Map<Tennisplayer, Tennisplayer> players2, Map<Tennisplayer, Tennisplayer> winner) {
         super(localDate, duration, score);
         this.players1 = players1;
         this.players2 = players2;
@@ -24,27 +39,27 @@ public class Doubles extends Tennismatch {
     // endregion
 
     // region Getter & Setter
-    public Pair<Tennisplayer, Tennisplayer> getPlayers1() {
+    public Map<Tennisplayer, Tennisplayer> getPlayers1() {
         return players1;
     }
 
-    public void setPlayers1(Pair<Tennisplayer, Tennisplayer> players1) {
+    public void setPlayers1(Map<Tennisplayer, Tennisplayer> players1) {
         this.players1 = players1;
     }
 
-    public Pair<Tennisplayer, Tennisplayer> getPlayers2() {
+    public Map<Tennisplayer, Tennisplayer> getPlayers2() {
         return players2;
     }
 
-    public void setPlayers2(Pair<Tennisplayer, Tennisplayer> players2) {
+    public void setPlayers2(Map<Tennisplayer, Tennisplayer> players2) {
         this.players2 = players2;
     }
 
-    public Pair<Tennisplayer, Tennisplayer> getWinner() {
+    public Map<Tennisplayer, Tennisplayer> getWinner() {
         return winner;
     }
 
-    public void setWinner(Pair<Tennisplayer, Tennisplayer> winner) {
+    public void setWinner(Map<Tennisplayer, Tennisplayer> winner) {
         this.winner = winner;
     }
     // endregion
