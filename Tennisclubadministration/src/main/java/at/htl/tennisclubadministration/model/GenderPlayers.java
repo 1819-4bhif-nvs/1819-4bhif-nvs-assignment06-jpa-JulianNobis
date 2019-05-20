fda35080ddafedf76d74854b0c8f2c959903ea6d
JulianNobis <julian09022001@gmail.com>
2019-05-20 20:25:19 +0200

    added gender enum

---
+++ Tennisclubadministration/src/main/java/at/htl/tennisclubadministration/model/GenderPlayers.java
@@ -0,0 +1,8 @@
+package at.htl.tennisclubadministration.model;
+
+public class GenderPlayers {
+    public enum Gender{ // muss so heißen, ansonsten gibt es einen error
+        MALE,
+        FEMALE
+    }
+}
